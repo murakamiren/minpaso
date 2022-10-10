830c71ffc5e963976a3a192ab51710884443b240
renmurakami <autoeyy@gmail.com>
2022-10-10 11:30:50 +0900

    feat: add navbar text

---
+++ src/components/navbar/navItem.ts
@@ -0,0 +1,6 @@
+import { NavItemType } from "./type";
+
+export const navItem: NavItemType[] = [
+	{ text: "探索する", href: "explore" },
+	{ text: "みんパソについて", href: "about" },
+];
--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,11 +1,23 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Link, List, ListItem } from "@chakra-ui/react";
+import NextLink from "next/link";
 import { FC } from "react";
+import { navItem } from "./navItem";
 
 const Navbar: FC = () => {
 	return (
-		<Box>
-			<Text>navbar</Text>
-		</Box>
+		<nav>
+			<Box>
+				<List display="flex" gap={4}>
+					{navItem.map((item, i) => (
+						<ListItem key={i}>
+							<NextLink href={item.href} passHref>
+								<Link>{item.text}</Link>
+							</NextLink>
+						</ListItem>
+					))}
+				</List>
+			</Box>
+		</nav>
 	);
 };
 
