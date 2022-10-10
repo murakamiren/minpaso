92af3922f0eec4861b3e552922aa5ecc472265b2
renmurakami <autoeyy@gmail.com>
2022-10-10 15:29:54 +0900

    feat: set react query

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "react-query";
 import theme from "../constant/theme";
+
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }: AppProps) {
 	return (
 		<ChakraProvider theme={theme}>
-			<Component {...pageProps} />
+			<QueryClientProvider client={client}>
+				<Component {...pageProps} />
+			</QueryClientProvider>
 		</ChakraProvider>
 	);
 }
