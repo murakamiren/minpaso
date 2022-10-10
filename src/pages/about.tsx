3be44f6dce819a21065dd403717f2ec554ba83cf
renmurakami <autoeyy@gmail.com>
2022-10-10 15:35:00 +0900

    feat: create page about

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import { Box, Heading } from "@chakra-ui/react";
+
+const About = () => {
+	return (
+		<main>
+			<Box>
+				<Heading as="h2">みんパソについて</Heading>
+			</Box>
+		</main>
+	);
+};
+
+export default About;
