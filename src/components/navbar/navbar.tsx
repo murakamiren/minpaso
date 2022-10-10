502d73dc2687d709ede35a572f20e57ab21af73a
renmurakami <autoeyy@gmail.com>
2022-10-10 13:26:34 +0900

    style: change h1 "パソ" text color

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Flex, Heading, Link, List, ListItem } from "@chakra-ui/react";
+import { Box, Button, chakra, Flex, Heading, Link, List, ListItem } from "@chakra-ui/react";
 import NextLink from "next/link";
 import { FC } from "react";
 import { navItem } from "./navItem";
@@ -7,8 +7,8 @@ const Navbar: FC = () => {
 	return (
 		<nav>
 			<Flex px={8} py={4} justify="space-between" alignItems="center">
-				<Heading as="h1" fontWeight="bold" color="text.black">
-					みんパソ
+				<Heading as="h1" fontWeight="bold" color="text.black" fontSize="3xl" letterSpacing="tighter">
+					みん<chakra.span color="linkedin.600">パソ</chakra.span>
 				</Heading>
 				<Box flex={1} display="flex" justifyContent="flex-end" alignItems="center" gap={4}>
 					<List display="flex" gap={4}>
