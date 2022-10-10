293f171b4097be38e2abb9b76273279b0c17c1df
renmurakami <autoeyy@gmail.com>
2022-10-10 16:51:23 +0900

    style: card grid adjust

--- src/components/cardGrid/cardGrid.tsx
+++ src/components/cardGrid/cardGrid.tsx
@@ -4,7 +4,13 @@ import Card from "../card/card";
 
 const CardGrid: FC = () => {
 	return (
-		<Grid gridTemplateColumns="repeat(auto-fit, minmax(200px, 1fr))" gap={16}>
+		<Grid gridTemplateColumns="repeat(auto-fit, minmax(300px, 1fr))" gap={16}>
+			<GridItem>
+				<Card src="https://placehold.jp/300x300.png" userName="test" postId="1" />
+			</GridItem>
+			<GridItem>
+				<Card src="https://placehold.jp/300x300.png" userName="test" postId="1" />
+			</GridItem>
 			<GridItem>
 				<Card src="https://placehold.jp/300x300.png" userName="test" postId="1" />
 			</GridItem>
