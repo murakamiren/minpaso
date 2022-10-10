5ceb55920e195a49c689617c0204dd5e99ba000e
renmurakami <autoeyy@gmail.com>
2022-10-10 11:09:05 +0900

    style: create theme

---
+++ src/constant/theme.ts
@@ -0,0 +1,12 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const colors = {
+	text: {
+		black: "#333",
+		white: "#fff",
+	},
+};
+
+const theme = extendTheme({ colors });
+
+export default theme;
