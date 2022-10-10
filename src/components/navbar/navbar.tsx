6340db81a7d81dcc5426104483daac58208bb1e3
renmurakami <autoeyy@gmail.com>
2022-10-10 11:16:00 +0900

    feat: create navbar component

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+import { Box, Text } from "@chakra-ui/react";
+import { FC } from "react";
+
+const Navbar: FC = () => {
+	return (
+		<Box>
+			<Text>navbar</Text>
+		</Box>
+	);
+};
+
+export default Navbar;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Box, Text } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import Head from "next/head";
+import Navbar from "../components/navbar/navbar";
 
 const Home: NextPage = () => {
 	return (
@@ -11,8 +12,10 @@ const Home: NextPage = () => {
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
+			<Navbar />
+
 			<Box display="flex" justifyContent="center">
-				<Text>hello world</Text>
+				<Text color="text.black">hello world</Text>
 			</Box>
 		</div>
 	);
