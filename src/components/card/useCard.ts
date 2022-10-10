c46a3602c25753b2061c41514cc3558e7d0d2f1f
renmurakami <autoeyy@gmail.com>
2022-10-10 16:46:46 +0900

    fix: infinite render

--- src/components/card/useCard.ts
+++ src/components/card/useCard.ts
@@ -1,12 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export const useCard = () => {
 	const [hover, isHover] = useState(false);
 	const [scale, setScale] = useState(1);
 	const [filter, setFilter] = useState("brightness(1)");
 
-	hover ? setScale(1.2) : setScale(1);
-	hover ? setFilter("brightness(1.1)") : setFilter("brightness(1)");
+	useEffect(() => {
+		if (hover) {
+			setScale(() => 1.2);
+			setFilter(() => "brightness(1.1)");
+		} else {
+			setScale(() => 1);
+			setFilter(() => "brightness(1)");
+		}
+	}, [hover]);
 
 	return { scale, filter, isHover };
 };
