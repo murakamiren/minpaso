23638bb574ad808e2a08311f169cfcb438a987ff
renmurakami <autoeyy@gmail.com>
2022-10-10 22:31:21 +0900

    feat: back index page from share page via back arrow

--- src/pages/share.tsx
+++ src/pages/share.tsx
@@ -1,11 +1,18 @@
+import { ArrowBackIcon } from "@chakra-ui/icons";
 import { Box, Heading } from "@chakra-ui/react";
 import { NextPageWithLayout } from "next";
+import NextLink from "next/link";
 import ShareForm from "../components/shareForm/shareForm";
 
 const Share: NextPageWithLayout = () => {
 	return (
 		<main>
 			<Box px={20}>
+				<NextLink href="/" passHref>
+					<a>
+						<ArrowBackIcon w={8} h={8} />
+					</a>
+				</NextLink>
 				<Heading as="h2">共有する</Heading>
 				<ShareForm />
 			</Box>
