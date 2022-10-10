cdc3b46cd6f4abadbef4802c2cb634aac0749a30
renmurakami <autoeyy@gmail.com>
2022-10-10 21:21:45 +0900

    feat: create share page

---
+++ src/pages/share.tsx
@@ -0,0 +1,14 @@
+import { Box, Heading } from "@chakra-ui/react";
+import { NextPageWithLayout } from "next";
+
+const Share: NextPageWithLayout = () => {
+	return (
+		<main>
+			<Box>
+				<Heading as="h2">共有する</Heading>
+			</Box>
+		</main>
+	);
+};
+
+export default Share;
