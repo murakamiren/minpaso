0596f7b30894e1728e75ffb5069fa15a265d4814
renmurakami <autoeyy@gmail.com>
2022-10-10 13:39:20 +0900

    refactor: footer markup

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,11 +1,18 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, chakra, Heading, Text } from "@chakra-ui/react";
 import { FC } from "react";
 
 const Footer: FC = () => {
 	return (
 		<footer>
-			<Box>
-				<Text>footer</Text>
+			<Box px={8}>
+				<Heading as="h1" fontWeight="bold" color="text.black" fontSize="3xl" letterSpacing="tighter">
+					みん<chakra.span color="linkedin.600">パソ</chakra.span>
+				</Heading>
+				<Text color="text.black" mt={4}>
+					自慢のPCを共有、探索しよう
+					<br />
+					PC好きの為の情報共有コミュニティー
+				</Text>
 			</Box>
 		</footer>
 	);
