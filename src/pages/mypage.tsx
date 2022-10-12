f26204d4ecf3fb2370897671b16e88ef861bc459
renmurakami <autoeyy@gmail.com>
2022-10-12 16:36:40 +0900

    feat: my page profile

--- src/pages/mypage.tsx
+++ src/pages/mypage.tsx
@@ -1,16 +1,38 @@
-import { Box, Heading } from "@chakra-ui/react";
+import { Avatar, Box, Center, Heading, Spinner, VStack } from "@chakra-ui/react";
 import { NextPageWithLayout } from "next";
+import MyPostGridMemo from "../components/cardGrid/myPostsGrid";
+import { useUser } from "../hook/useUser";
 import Layout from "../layout/layout";
 
 const MyPage: NextPageWithLayout = () => {
+	const { user } = useUser();
+
+	const userPhoto = user?.photoURL as string;
+
+	if (!user)
+		return (
+			<Center>
+				<Spinner />
+			</Center>
+		);
+
 	return (
-		<>
-			<main>
-				<Box>
-					<Heading as="h2">マイページ</Heading>
+		<main>
+			<Box px={20} mt={20}>
+				<VStack spacing={2}>
+					<Avatar src={userPhoto} size="xl" />
+					<Heading as="h2">{user.displayName}</Heading>
+				</VStack>
+				<Center mt={12}>
+					<Heading as="h3" fontSize="2xl">
+						投稿一覧
+					</Heading>
+				</Center>
+				<Box mt={12}>
+					<MyPostGridMemo user={user} />
 				</Box>
-			</main>
-		</>
+			</Box>
+		</main>
 	);
 };
 
