5256a2ea710a46404efe4011242ec6e226873b42
renmurakami <autoeyy@gmail.com>
2022-10-15 19:13:53 +0900

    feat: useUser isLoading

--- src/hook/useUser.ts
+++ src/hook/useUser.ts
@@ -2,7 +2,7 @@ import { useAuthUser } from "@react-query-firebase/auth";
 import { auth } from "../lib/firebase";
 
 export const useUser = () => {
-	const { data: user } = useAuthUser("[nav-user]", auth, { staleTime: Infinity });
+	const { data: user, isLoading } = useAuthUser("[nav-user]", auth, { staleTime: Infinity });
 
-	return { user };
+	return { user, isLoading };
 };
