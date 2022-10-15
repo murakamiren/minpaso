1cc803d28aa0443afa7606b69093976611546526
renmurakami <autoeyy@gmail.com>
2022-10-15 21:50:02 +0900

    style: detail heading

--- src/components/detail/detail.tsx
+++ src/components/detail/detail.tsx
@@ -1,5 +1,5 @@
 import { ArrowBackIcon } from "@chakra-ui/icons";
-import { Box, Button, Center, Image, Spinner, Text } from "@chakra-ui/react";
+import { Box, Button, Center, Heading, Image, Spinner, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 import { FC } from "react";
 import { Spec } from "../../types/post";
@@ -19,22 +19,31 @@ const Detail: FC = () => {
 	return (
 		<Box>
 			<ArrowBackIcon onClick={() => router.back()} cursor="pointer" w={8} h={8} />
+			<Heading as="h2">{postData?.title}</Heading>
 			{postData?.image.map((img, i) => (
 				<Box key={i} w="200px">
 					<Image src={img.src} alt="pc" w="full" h="full" />
 				</Box>
 			))}
-			<Text>{postData?.title}</Text>
-			<Text>{postData?.author}</Text>
-			<Text>{postData?.point}</Text>
-			{Object.keys(postData.spec).map((part) => {
-				const key = part as keyof Spec;
-				return (
-					<Text key={part}>
-						{part}: {postData.spec[key]}
-					</Text>
-				);
-			})}
+			<Box>
+				<Heading as="h3">投稿者</Heading>
+				<Text>{postData?.author}</Text>
+			</Box>
+			<Box>
+				<Heading as="h3">こだわりポイント</Heading>
+				<Text>{postData?.point}</Text>
+			</Box>
+			<Box>
+				<Heading as="h3">パーツ</Heading>
+				{Object.keys(postData.spec).map((part) => {
+					const key = part as keyof Spec;
+					return (
+						<Text key={part}>
+							{part}: {postData.spec[key]}
+						</Text>
+					);
+				})}
+			</Box>
 			{user?.uid === postData.authorId && (
 				<Button
 					colorScheme="red"
